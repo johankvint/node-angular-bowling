eab11ededb17c83302073d3f66a1033e70857ff1
Johan Kvint <johan@kompilera.se>
2018-09-09 00:10:51 +0200

    initial matches module

---
+++ client/src/app/matches/matches.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatchesService } from './matches.service';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [],
+  providers: [
+    MatchesService
+  ]
+})
+export class MatchesModule { }
